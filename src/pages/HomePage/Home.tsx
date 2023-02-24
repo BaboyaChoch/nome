41999ab4695286e2a51de995f44479d1e2da5829
Baboya Choch <baboyachoch@gmail.com>
2023-02-24 15:56:27 -0600

    setup prettier and tailwind

---
+++ src/pages/HomePage/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Home() {
+	return (
+		// create a centered div with a 3xl font and underline
+		<div className="center">
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+		</div>
+	);
+}
